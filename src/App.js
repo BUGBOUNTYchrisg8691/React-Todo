3edab3a92d99629f5e99d4e29c1371f5c51c3e84
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-24 12:54:23 -0400

    search functionality for stretch done

--- src/App.js
+++ src/App.js
@@ -1,63 +1,97 @@
-import React from 'react';
+import React from "react";
 
-import "./App.css"
+import "./App.css";
 
-import TodoList from "./components/TodoList"
+import TodoList from "./components/TodoList";
+import Search from "./components/Search";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
-    super()
+    super();
     // this.addTodo.bind(this)
     this.state = {
-      todos: [] 
-    } 
+      todos: [],
+      todosCopy: [],
+      search: "",
+    };
   }
- 
+
+  onSubmit = () => {
+    if (this.state.search === "") {
+      this.setState({
+        todos: [...this.state.todosCopy],
+        todosCopy: [],
+      });
+    } else {
+      this.setState({
+        todosCopy: [...this.state.todos],
+        todos: this.state.todos.filter((todo) => {
+          return todo.todo
+            .toLowerCase()
+            .includes(this.state.search.toLowerCase());
+        }),
+      });
+    }
+  };
+
+  onChange = (e) => {
+    this.setState({
+      todos: [...this.state.todos],
+      search: e.target.value,
+    });
+  };
+
   addTodo = (e) => {
-    e.preventDefault()
-    if (this._inputRef.value !== '') {
+    e.preventDefault();
+    if (this._inputRef.value !== "") {
       let newTodo = {
         todo: this._inputRef.value,
         key: Date.now(),
-        completed: false
-      }
+        completed: false,
+      };
 
       this.setState((prevState) => {
         return {
-          todos: prevState.todos.concat(newTodo)
-        }
-      })
+          todos: prevState.todos.concat(newTodo),
+        };
+      });
 
-      this._inputRef.value = ''
+      this._inputRef.value = "";
     }
-    console.log(this.state.todos)
-  }
+    console.log(this.state.todos);
+  };
 
-  toggleTodo = key => {
-    const newTodos = [...this.state.todos]
-    const todo = newTodos.find(todo => todo.key === key)
-    todo.completed = !todo.completed
-    this.setState({ todos: newTodos })
-  }
+  toggleTodo = (key) => {
+    const newTodos = [...this.state.todos];
+    const todo = newTodos.find((todo) => todo.key === key);
+    todo.completed = !todo.completed;
+    this.setState({ todos: newTodos });
+  };
 
   clearTodos = () => {
-    const newTodos = this.state.todos.filter(todo => !todo.completed)
-    this.setState({ todos: newTodos })
-  }
+    const newTodos = this.state.todos.filter((todo) => !todo.completed);
+    this.setState({ todos: newTodos });
+  };
 
   render() {
     return (
       <div className="todo-list-main">
         <h1>Todos</h1>
-        <form onSubmit={ this.addTodo }>
-          <input type="text" ref={ (todo) => this._inputRef = todo } placeholder="Enter Todo..." />
+        Search Todos
+        <Search onSubmit={this.onSubmit} onChange={this.onChange} />
+        <form onSubmit={this.addTodo}>
+          <input
+            type="text"
+            ref={(todo) => (this._inputRef = todo)}
+            placeholder="Enter Todo..."
+          />
           <button type="submit">Add Todo</button>
         </form>
-        <TodoList todos={ this.state.todos } toggleTodo={ this.toggleTodo } />
-        <button onClick={ this.clearTodos }>Clear Completed</button>
+        <TodoList todos={this.state.todos} toggleTodo={this.toggleTodo} />
+        <button onClick={this.clearTodos}>Clear Completed</button>
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Search extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="search" onChange={this.props.onChange} />
+        <button>Search</button>
+      </form>
+    );
+  }
+}
